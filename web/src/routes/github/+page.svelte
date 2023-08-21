24bf1b663628f0449fddcd2b3f3c6f8bba03e427
Robin Schneider <rschneider@vwd.com>
2023-08-21 18:33:36 +0200

    add skills

--- web/src/routes/github/+page.svelte
+++ web/src/routes/github/+page.svelte
@@ -1,38 +1,35 @@
 <h1 class="text-center uppercase mt-5">my github repositories</h1>
 <div class="h-full mx-auto mt-5">
 	<div class="flex flex-row flex-wrap justify-center">
-	{#each repos as repo}
-	{#if !repo.fork}
-	<div>
-	</div>
-	<div class="card p-4 ml-4 mb-4" style="width: 400px;">
-		{#if repo.language}
-			<p class="text-center mb-1 text-cyan-400">{repo.language}</p>
-		{:else}
-		<br>
-		{/if}
-		<h1 class="text-center">{repo.name}</h1>
-		<div class="justify-center text-center mt-4 mb-4">
-			<a class="btn bg-primary-hover-token border-2 border-cyan-400 text-cyan-400" href="{repo.html_url}" target="_blank">
-				<IconBrandGithub />
-				<p class="uppercase">open</p>
-			</a>
-			<button use:clipboard="{repo.ssh_url}" class="btn bg-primary-hover-token border-2 border-cyan-400 text-cyan-400">
-				<IconBrandGit />
-				<!-- {copied ? 'Copied 👍' : 'Copy'} -->
-				<p class="uppercase">clone</p>
-			</button>
-			<button class="btn bg-primary-hover-token border-2 border-cyan-400 text-cyan-400">
-				<IconStar />
-				<p>
-					{repo.stargazers_count}
-				</p>
-			</button>
+		{#each repos as repo}
+		{#if !repo.fork}
+		<div class="card p-4 ml-2 mr-2 mt-4" style="width: 400px;">
+			{#if repo.language}
+				<p class="text-center text-cyan-400">{repo.language}</p>
+			{:else}
+			<br>
+			{/if}
+			<h2 class="text-center">{repo.name}</h2>
+			<div class="justify-center text-center mt-4 mb-4">
+				<a class="btn bg-primary-hover-token border-2 border-cyan-400 text-cyan-400" href="{repo.html_url}" target="_blank">
+					<IconBrandGithub />
+					<p class="uppercase">open</p>
+				</a>
+				<button use:clipboard="{repo.ssh_url}" class="btn bg-primary-hover-token border-2 border-cyan-400 text-cyan-400">
+					<IconBrandGit />
+					<!-- {copied ? 'Copied 👍' : 'Copy'} -->
+					<p class="uppercase">clone</p>
+				</button>
+				<button class="btn bg-primary-hover-token border-2 border-cyan-400 text-cyan-400">
+					<IconStar />
+					<p>
+						{repo.stargazers_count}
+					</p>
+				</button>
+			</div>
 		</div>
-		<h5>UPDATE: {repo.updated_at}</h5>
-	</div>
-	{/if}
-	{/each}
+		{/if}
+		{/each}
 	</div>
 </div>
 
--- web/src/routes/skills/+page.svelte
+++ web/src/routes/skills/+page.svelte
@@ -1,12 +1,30 @@
+<h1 class="text-center uppercase mt-5">My Technical skills</h1>
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="text-center flex flex-row items-center">
+	<div class="flex flex-col items-center" style="width: 100%;">
 	{#each skills as skill}
-		<div class="card p-4 ml-4">{skill.name}</div>
+		<div class="card p-8 mt-4" style="width: 90%;">
+			<div class="flex flex-row">
+				<div class="text-left" style="width: 100%;">
+					<h1 class="text-cyan-400 mb-6">{skill.name}</h1>
+					<p>{skill.text}</p>
+				</div>
+				<p style="display:inline-block; width: 100%;"></p>
+				<div class="flex flex-row items-center">
+					<a class="btn bg-primary-hover-token border-2 border-cyan-400 text-cyan-400 h-14 mr-4" href="{skill.gitlink}">
+						<IconBrandGithub />
+						<p class="uppercase">open</p>
+					</a>
+					<ProgressRadial class="fill-cyan-400" meter="stroke-cyan-400" track="stroke-cyan-400/30" fill font=150 width="w-14" value={skill.level}>{skill.level}%</ProgressRadial>
+				</div>
+			</div>
+		</div>
 	{/each}
 	</div>
 </div>
 
 <script>
+	import { IconBrandGithub } from '@tabler/icons-svelte';
+	import { ProgressRadial } from "@skeletonlabs/skeleton";
 	let skills = [];
 
 	async function get_skills() {
